ea9647d071728f24fda0773b18412f5e2ae4b206
Abhishek Balaji <abhishek.balaji2@gmail.com>
2015-06-15 07:21:43 +0530

    Initial commit

--- prog2.sh
+++
@@ -1,6 +0,0 @@
-echo Enter the two numbers
-read a b
-b=` expr $a \* $b `
-a=` expr $b / $a `
-b=` expr $b / $a ` 
-echo a=$a b=$b
--- test.sh
+++
@@ -1,8 +0,0 @@
-echo "Enter a number"
-read a
-num=1
-echo $a
-for (( num=1, $num -lt $a; num++ ))
-do
-echo Number:$num
-done
